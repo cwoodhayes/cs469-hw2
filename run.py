07a3ab928f8aa44e72a6e8e87f57e8fc022f3995
conorbot <cwoodhayes@gmail.com>
2025-11-16 16:28:13 -0600

    continuing to mess with seaborn barplot

--- run.py
+++ run.py
@@ -64,21 +64,21 @@ def get_cli_args() -> argparse.Namespace:
 
 
 def partA1(ds: Dataset):
-    ds = ds.segment_percent(0, 1, True)
+    # ds = ds.segment_percent(0, 1, True)
     obs = ObservabilityData.from_dataset(ds)
-    obs.to_file()
+    # obs.to_file()
 
     # plot ground-truth observability dataset
     # fig = plt.figure()
     # plot_trajectories_pretty(ds, fig, "Landmark Observability (Ground Truth)")
 
-    fig2 = plt.figure()
-    ax = fig2.subplots()
+    # fig2 = plt.figure()
+    # ax = fig2.subplots()
     # row = obs.data.iloc[100]
     # plot_single_observation(ds, obs, ax, row)
     # ax.legend(fontsize=8)
 
-    plot_landmark_bars(ds, obs, ax)
+    plot_landmark_bars(ds, obs)
 
 
 if __name__ == "__main__":
