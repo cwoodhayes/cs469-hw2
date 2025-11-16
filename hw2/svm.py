d8f81f5874c2ecb6ef2eefcff59235f8959b22ff
conorbot <cwoodhayes@gmail.com>
2025-11-16 12:03:28 -0600

    added a nice test to fail til this works

--- hw2/svm.py
+++ hw2/svm.py
@@ -1,3 +1,57 @@
 """
 Support Vector Machine implementations.
 """
+
+import numpy as np
+
+
+class SVMError(Exception):
+    """Base error for SVM module."""
+
+    pass
+
+
+class SVM:
+    """
+    Linear-kernel support vector machine.
+
+    Implemented following the paper:
+
+    @inproceedings{Fletcher2010SupportVM,
+        title={Support Vector Machines Explained},
+        author={Tristan Fletcher},
+        year={2010},
+        url={https://api.semanticscholar.org/CorpusID:218451389}
+    }
+
+    With API inspiration taken from
+    https://scikit-learn.org/stable/modules/svm.html
+
+    """
+
+    def __init__(self) -> None:
+        self._fitted = False
+
+    def fit(self, X: np.ndarray, y: np.ndarray) -> None:
+        """Train the classifier on a dataset.
+
+        Args:
+            X (np.ndarray): training data, shape=(n_samples, n_features)
+            y (np.ndarray): training labels, shape=(n_samples,)
+        """
+
+        self._fitted = True
+
+    def predict(self, x: np.ndarray) -> np.ndarray:
+        """Perform classification on inputs x
+
+        Args:
+            x (np.ndarray): one or many inputs x of shape (n_samples, n_features)
+
+        Returns:
+            np.ndarray: output classes, of shape (n_samples)
+        """
+        if not self._fitted:
+            raise SVMError("fit() has not been called.")
+
+        return np.zeros(x.shape[0])
---
+++ hw2/test_svm.py
@@ -0,0 +1,33 @@
+"""
+A few simple tests for svm.py
+"""
+
+import numpy as np
+
+from hw2.svm import SVM
+
+
+def test_svm_basic():
+    clf = SVM()
+
+    # make a stupid simple linearly separable 2D dataset
+    xtrain_a = [[1, 0], [2, 0], [3, 0]]
+    xtrain_b = [[-1, 5], [0, 5], [1, 5]]
+    X = np.array(xtrain_a + xtrain_b, dtype="float")
+    y = np.array([0] * len(xtrain_a) + [1] * len(xtrain_b), dtype="int")
+
+    clf.fit(X, y)
+
+    # poke at some test points
+    xtest_a = [
+        [1.5, 0],
+        [4, 0],
+        [4, -4],
+        [3, -400],
+    ]
+    xtest_b = [[1.5, 5], [1.5, 100], [4, 6]]
+    xtest = np.array(xtest_a + xtest_b, dtype="float")
+    ytest = np.array([0] * len(xtest_a) + [1] * len(xtest_b), dtype="int")
+
+    yhat = clf.predict(xtest)
+    np.testing.assert_allclose(yhat, ytest)
